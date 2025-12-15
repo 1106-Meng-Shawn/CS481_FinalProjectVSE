706363b224438a6a447e85c58cfe194cf6757cbb
1102-Luong-Vincent <vinceluong2@gmail.com>
2025-12-15 11:26:57 -0800

    added credits

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject SettingsPanel;
+    public GameObject creditsPanel;
     public GameObject mainMenu;
     [SerializeField] public Button SettingsButton;
     [SerializeField] public Button PlayButton;
     [SerializeField] public Button QuitButton;
+    [SerializeField] public Button CreditsButton;
     [SerializeField] public Button BackButton;
+    [SerializeField] public Button CreditsBackButton;
     [SerializeField] public Slider musicSlider;
     [SerializeField] public Slider sfxSlider;
     bool isSettingsOpen = false;
+    bool isCreditsOpen = false; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void Awake()
@@ -21,6 +25,8 @@ public class MainMenu : MonoBehaviour
         SettingsButton.onClick.AddListener(Settings);
         BackButton.onClick.AddListener(Back);
         QuitButton.onClick.AddListener(Quit);
+        CreditsButton.onClick.AddListener(Credits);
+        CreditsBackButton.onClick.AddListener(CreditsBack);
         musicSlider.onValueChanged.AddListener(musicChange);
         sfxSlider.onValueChanged.AddListener(sfxChange);
 
@@ -47,12 +53,25 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(true);
     }
 
+    void CreditsBack()
+    {
+        VolumeManager.Instance.PlayButtonClick();
+        creditsPanel.SetActive(false);
+        mainMenu.SetActive(true);
+    }
     void Quit()
     {
         VolumeManager.Instance.PlayButtonClick();
         Application.Quit();
     }
 
+    void Credits()
+    {
+        VolumeManager.Instance.PlayButtonClick();
+        SettingsPanel.SetActive(false);
+        mainMenu.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
     void musicChange(float value)
     {
         VolumeManager.Instance?.SetMusicVolume(value);
